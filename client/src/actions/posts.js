19d1dd45fb37dc639ca1ba62cb4cc9a94d5c0e14
molla-12 <mollaman19@gmail.com>
2022-10-04 10:20:20 +0300

    first commit

---
+++ client/src/actions/posts.js
@@ -0,0 +1,37 @@
+import * as api from '../api';
+
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.feachPosts();
+        dispatch({ type: 'FEATCH_ALL', payload: data });
+
+    } catch (error) {
+        console.log(error.message);
+
+    }
+    //dispatch(action);
+    // const action = { type: 'FEATCH_ALL', payload: [] }
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+        dispatch({ type: 'CREATE', payload: data });
+
+    } catch (error) {
+        console.log(error.message);
+
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id, post);
+
+        dispatch({ type: 'UPDATE', payload: data });
+
+    } catch (error) {
+        console.log(error.message);
+
+    }
+}
---
+++ server/controllers/posts.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send('Not Post found with this Id');
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+    return res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Post found with this Id');
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post Deleted Successfuly' });
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!req.userId) return res.status(404).json({ message: "Please login to like a post" });
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Paost found with this Id');
+
+    const post = await PostMessage.findById(id);
+
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    if (index === -1) {
+        post.likes.push(req.userId);
+    } else {
+        post.likes = post.likes.filter((id) => id != String(req.userId));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+
+    res.json(updatedPost);
+}
