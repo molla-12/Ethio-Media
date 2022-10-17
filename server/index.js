d63ec6873c955ded942e8ccd63f1eb3b0a68a30c
molla-12 <mollaman19@gmail.com>
2022-10-17 17:01:45 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -22,13 +22,13 @@ app.get('/', (req, res) => {
     res.send("app running");
 });
 
-if (process.env.CONNECTION_URL == "production") {
-    app.use(express.static("client/build"));
+// if (process.env.CONNECTION_URL == "production") {
+//     app.use(express.static("client/build"));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-    });
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+//     });
+// }
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
